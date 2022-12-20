465944179ef2bf6d7b725d215ff5d2ce80650648
trickster1o1 <nischaltuladhar15@gmail.com>
2022-12-20 13:13:20 +0545

    hanging man starter logic phase complete

--- test.py
+++ test.py
@@ -1,9 +1,47 @@
-def printAdd(a,b):
-    print('The Word Is '+str(a+b))
-    
-print('Enter a Number')
-b = int(input())
-print('Enter another')
-a = int(input())
+# Note: You get 5 attempts to guess it right
 
-printAdd(a,b)
+word = "nischal"
+print('#Note: You get 8 attempts to guess it right.')
+status = 0
+
+def checkWord(inp, hint):
+    count = 0
+    for w in word:
+        bhint = list(hint)
+        if(inp == w):
+            if(bhint[count] == inp):
+                return {'status':'fail', 'hint':''.join(bhint)}
+            bhint[count] = inp
+            return {'status': 'finish' if bhint.count('_') <= 0 else "success", 'hint':''.join(bhint)}
+        count = count+1
+    return 0
+
+def main():
+    hint = 'n__c__l'
+    limit = 0
+    print(hint)
+    print('Enter your guess: ')
+    while(limit <8):
+        uin = input()
+        chk = checkWord(uin, hint)
+        if(chk['status'] == 'success'):
+            print('~~~~~~~~~~~~ Right Answer ~~~~~~~~~~~')
+        elif(chk['status'] == 'finish'):
+            print('~*~*~*~*~*~*~*~* you\'ve wone *~*~*~*~*~*~*~*')
+            break
+        else:
+            print('******** Wrong Answer ********')
+        limit += 1
+        hint = chk['hint']
+        print(chk['hint'])
+        print('Enter your guess: ')
+
+
+
+    
+        
+
+
+
+
+main()

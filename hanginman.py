654504565300b9accfad908e11a6207478f04909
trickster1o1 <nischaltuladhar15@gmail.com>
2022-12-21 16:04:38 +0545

    multiplayer init

--- hanginman.py
+++ hanginman.py
@@ -1,6 +1,17 @@
 # Note: You get 5 attempts to guess it right
-
+import random
 class HangingMan:
+    def selMode():
+        print("Select Mode: \n 1) Single Player \n 2) Multi player")
+        mode = input()
+        try:
+            if(int(mode) < 1 or int(mode) > 2):
+                print('Just select 1 or 2 god dammit')
+                return HangingMan.defLevel()
+            return int(mode)
+        except: 
+            print('Just select 1 or 2 god dammit')
+            return HangingMan.selMode()
     def defLevel():
         print('Enter Difficulty: \n 1) Easy \n 2) Intermidiate \n 3) Hard')
         level = input()
@@ -30,10 +41,23 @@ class HangingMan:
         return {'status':s, 'hint':''.join(bhint)}
 
     def main():    
-        level = HangingMan.defLevel()
-        word = "tomato" if level == 1 else "bonfire" if level == 2 else "befuddled" 
+        mode = HangingMan.selMode()
+        level = 0 if mode == 2 else HangingMan.defLevel()
+        if(level == 0):
+            print('Enter your word:')
+        word = "tomato" if level == 1 else "bonfire" if level == 2 else "befuddled" if level == 3 else input()
         print('#Note: You get 8 attempts to guess it right.')
-        hint = '__ma__' if level == 1 else "____i_e" if level == 2 else "b_f______"
+        if(level == 0):
+            cHint = []
+            for w in word:
+                cHint.append('_')
+            c = 0
+            while(c != 2):
+                flag = random.randint(0,len(word)-1)
+                cHint[flag] = word[flag]
+                c += 1
+
+        hint = '__ma__' if level == 1 else "____i_e" if level == 2 else "b_f______" if level == 3 else cHint
         limit = 0
         status = 0
         life = '00000000'
